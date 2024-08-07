9ac6cba40cb9aefc0f61c85dc4ebbcbf1a535f33
saviomds <dominiquesavio2003@gmail.com>
2024-08-07 15:26:46 +0200

    Live update

---
+++ Views/pages/Js Interaction Design /live.js
@@ -0,0 +1,9 @@
+document.querySelector(".join").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href =
+    "https://discord.com/channels/1270733327573323826/1270733327573323828";
+});
+document.querySelector(".ignore").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "../Welcome/index.html";
+});
